fc22e59b98e1784257c60bb612e091167027b03b
Yuta Yotsumoto <y.2000529@gmail.com>
2021-06-01 10:15:42 +0900

    デフォルト

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Screen from './screens/Screen'; //
+import Screen from './screens/Screen'; //srcフォルダの screensフォルダの　Screen.js をimport
 
 const App = () => {
-  return <Screen />;
+  return <Screen />; //Screenコンポネントを出力
 };
 
 export default App; //
--- src/screens/Screen.js
+++ src/screens/Screen.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React from 'react'; //Reactを宣言
+import {StyleSheet, SafeAreaView, Text, TouchableOpacity} from 'react-native'; //imoportするものを表記
 
+// import Header from '../components/Header';
+
+//functional component(arrow関数)
 const Screen = () => {
   return (
     <SafeAreaView>
-      <Text>Screen.js</Text>
+      <Text style={styles.text}>Counter</Text>
+      <Text>0</Text>
+
+      <TouchableOpacity>
+        <Text style={styles.button}>+</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.button}>-</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text>リセット</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
-export default Screen;
+//各要素に対してstyleSheetコンポネントで装飾
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: 'tomato',
+  },
+  text: {
+    color: 'black',
+  },
+  button: {
+    color: 'black',
+  },
+});
+
+export default Screen; //App.js でimportするためにexport
