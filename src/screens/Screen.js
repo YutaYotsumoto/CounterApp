7b8fbc4b50eb63ddaef8220a7864f211c7f4b6a1
Yuta Yotsumoto <y.2000529@gmail.com>
2021-06-03 12:03:49 +0900

    SafeAreaViewでのステータスバーの着色 - <SafeAreaView>に対してbackgroundColor = 'skyblue' - header以下の要素を<View>で囲いbackgroundColor = 'white'

--- src/screens/Screen.js
+++ src/screens/Screen.js
@@ -1,72 +1,82 @@
+/* eslint-disable no-undef */
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react'; //Reactを宣言
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native'; //imoportするものを表記
-
-// import Header from '../components/Header';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'; //imoportするものを表記
 
 //functional component(arrow関数)
 const Screen = () => {
   return (
-    <View>
-      {/* -- < HeaderPart > --------------------------------------------------------------------------------------------------------------------------*/}
+    <SafeAreaView backgroundColor="skyblue">
+      <View backgroundColor="white">
+        {/* -- < HeaderPart > --------------------------------------------------------------------------------------------------------------------------*/}
+        <View style={{width: '100%'}} backgroundColor="skyblue">
+          <Text
+            style={[
+              styles.text,
+              {fontSize: 35, color: 'white', marginBottom: 5},
+            ]}>
+            Counter
+          </Text>
+        </View>
 
-      <View style={{padding: 10, width: '100%'}} backgroundColor="skyblue">
-        <Text style={[styles.text, {color: 'white', marginTop: 30}]}>
-          Counter
-        </Text>
-      </View>
+        {/* -- < counterPart > -------------------------------------------------------------------------------------------------------------------*/}
 
-      {/* -- < counterPart > -------------------------------------------------------------------------------------------------------------------*/}
+        <View
+          style={{
+            paddingTop: 150,
+            paddingBottom: 150,
+            backgroundColor: 'yellow',
+            marginBottom: 50,
+          }}>
+          <Text style={[styles.text, {fontSize: 100}]}>0</Text>
+        </View>
 
-      <View
-        style={{
-          paddingTop: 150,
-          paddingBottom: 150,
-          backgroundColor: 'yellow',
-          marginBottom: 50,
-        }}>
-        <Text style={[styles.text, {fontSize: 100}]}>0</Text>
-      </View>
+        {/* -- < buttonPart > -------------------------------------------------------------------------------------------------------------------------- */}
 
-      {/* -- < buttonPart > -------------------------------------------------------------------------------------------------------------------------- */}
-
-      <View
-        style={[
-          styles.flexbox,
-          {
-            justifyContent: 'space-around', //justifyContentを　'center' から 'space-around' へ上書き
-            marginTop: 50,
-          },
-        ]}>
-        <TouchableOpacity style={styles.buttonBase}>
-          <Text style={[styles.text, {fontSize: 30, paddingTop: 10}]}>-</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.buttonBase}>
-          <Text style={[styles.text, {fontSize: 30, paddingTop: 10}]}>+</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* -- < resetButtonPart > -------------------------------------------------------------------------------------------------------------------------- */}
-
-      <View
-        style={[
-          styles.flexbox,
-          {
-            marginTop: 100,
-          },
-        ]}>
-        <TouchableOpacity
+        <View
           style={[
-            styles.buttonBase,
+            styles.flexbox,
             {
-              width: 100, //buttonBaseの　widthを60　から 100　へ上書き
+              justifyContent: 'space-around', //justifyContentを　'center' から 'space-around' へ上書き
+              // marginTop: 50,
             },
           ]}>
-          <Text style={[styles.text, {paddingTop: 17}]}>リセット</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonBase}>
+            <Text style={[styles.text, {fontSize: 30, paddingTop: 10}]}>-</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonBase}>
+            <Text style={[styles.text, {fontSize: 30, paddingTop: 10}]}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* -- < resetButtonPart > -------------------------------------------------------------------------------------------------------------------------- */}
+
+        <View
+          style={[
+            styles.flexbox,
+            {
+              marginTop: 100,
+            },
+          ]}>
+          <TouchableOpacity
+            style={[
+              styles.buttonBase,
+              {
+                width: 100, //buttonBaseの　widthを60　から 100　へ上書き
+              },
+            ]}>
+            <Text style={[styles.text, {paddingTop: 17}]}>リセット</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -90,67 +100,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-
-  safeArea: {
-    // backgroundColor: 'tomato',
-  },
-
-  header: {
-    // padding: 10,
-    // width: '100%',
-    // backgroundColor: 'skyblue',
-  },
-
-  headerText: {
-    // color: 'white',
-    // fontSize: 25,
-    // fontWeight: 'bold',
-    // textAlign: 'center',
-    // marginTop: 30,
-  },
-
-  counterPart: {
-    // paddingTop: 150,
-    // paddingBottom: 150,
-    // backgroundColor: 'yellow',
-    // marginBottom: 50,
-  },
-
-  counterDigit: {
-    // fontWeight: 'bold',
-    // fontSize: 100,
-    // textAlign: 'center',
-  },
-
-  buttonsPart: {
-    // flex: 1,
-    // flexDirection: 'row', //子要素を横並び　かつ　並び順(右左)を反対
-    // alignItems: 'center', //垂直方向
-    // justifyContent: 'space-around', //水平方向
-    // width: '100%',
-    // marginTop: 50,
-  },
-
-  resetPart: {
-    // marginTop: 100,
-    // flex: 1,
-    // flexDirection: 'row',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  resetButtonBase: {
-    // width: 100,
-    // height: 50,
-    // borderRadius: 5,
-    // backgroundColor: 'tomato',
-  },
-  resetText: {
-    // fontWeight: 'bold',
-    // fontSize: 25,
-    // color: 'black',
-    // textAlign: 'center',
-    // paddingTop: 13,
   },
 });
 
